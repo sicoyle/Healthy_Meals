3dea0b8fce987e30f108575d42aa8b85da2e0cac
Brent Redmon <btr26@txstate.edu>
2019-03-23 13:04:34 -0500

    Created the home page with Sam and Cass

--- app/routes.py
+++ app/routes.py
@@ -25,7 +25,7 @@ api = Api(app)
 # TODO - LOGIN!
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    return render_template('Front_Page.html')
 
 # 
 
