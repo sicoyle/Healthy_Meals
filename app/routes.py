2bc4328301ebc66df5860835681e663df197fbfd
Sam <samc@txstate.edu>
2019-04-02 19:17:41 -0500

    Added user profile route

--- app/routes.py
+++ app/routes.py
@@ -65,6 +65,15 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    
+    #add other user information here!!
+
+    return render_template('user.html', user=user)
+
 @app.route('/logout')
 def logout():
     logout_user()
