515038395755ae614575877c65e58cae5c64f5a4
zacharygolla <ztg5@txstate.edu>
2019-04-21 10:19:05 -0500

    Added incrementer/decrementer code to the cart page for quantity (backend still needs to be attached).

---
+++ app/static/js/cart.js
@@ -0,0 +1,11 @@
+$('.btn-items-decrease').on('click', function () {
+    var input = $(this).siblings('.input-items');
+    if (parseInt(input.val(), 10) >= 1) {
+        input.val(parseInt(input.val(), 10) - 1);
+    }
+});
+
+$('.btn-items-increase').on('click', function () {
+    var input = $(this).siblings('.input-items');
+    input.val(parseInt(input.val(), 10) + 1);
+});
