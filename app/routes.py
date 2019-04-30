9734deaa73053f6ed3135034b6a5c36f9009c76a
Sam <samc@txstate.edu>
2019-04-30 15:49:52 -0500

    Added customized error messages for login page

--- app/routes.py
+++ app/routes.py
@@ -116,20 +116,24 @@ def gift():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    error = None
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
         user = UserModel.query.filter_by(email=form.email.data).first()
         if user is None or not user.check_password(form.password.data):
+            error = 'Incorrect password.'
+            if user is None:
+                error = 'User not found. Please try a different email address.'
             flash('Invalid username or password')
-            return redirect(url_for('login'))
+            return render_template('login.html', title='Sign In', form=form, error=error)
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
         return redirect(next_page)
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('login.html', title='Sign In', form=form, error=error)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
